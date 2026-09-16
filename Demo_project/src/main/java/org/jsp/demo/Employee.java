0d6acca68ddc999c77e601f68c6ce65a70907057
Prem Kumar Shaw <277076306+premkumarshaw04@users.noreply.github.com>
2026-09-16 09:53:59 +0530

    Created table in using hibernate

---
+++ Demo_project/src/main/java/org/jsp/demo/Employee.java
@@ -0,0 +1,27 @@
+package org.jsp.demo;
+
+public class Employee {
+	private int id;
+	private String name;
+	private double salary;
+	//getter and setter methods
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
