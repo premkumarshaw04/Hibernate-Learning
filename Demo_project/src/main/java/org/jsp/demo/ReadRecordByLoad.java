e116337d43c7293acef6e1d908eddf94c77ebb6a
Prem Kumar Shaw <277076306+premkumarshaw04@users.noreply.github.com>
2026-09-18 09:43:47 +0530

    Read Record by Get and Load

---
+++ Demo_project/src/main/java/org/jsp/demo/ReadRecordByLoad.java
@@ -0,0 +1,21 @@
+package org.jsp.demo;
+
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ReadRecordByLoad {
+	public static void main(String[] args) {
+		Configuration conf = new Configuration();
+		conf.configure();
+		SessionFactory sef = conf.buildSessionFactory();
+		Session ses = sef.openSession();
+		
+		System.out.println("Enter the Primary Key: ");
+		Employee e = ses.load(Employee.class, new Scanner(System.in).nextInt());
+		System.out.println(e.getId());
+		System.out.println(e.getName());
+	}
+}
